64a86929a1583b3c5258dd00bcda7663d9206e44
Zouhir Amrani <zouhiramrani397@gmail.com>
2023-11-09 19:10:10 +0100

    Validation forms

--- src/ForrmsValidation/FormValidation.jsx
+++ src/ForrmsValidation/FormValidation.jsx
@@ -12,20 +12,20 @@ export default function FormsValidation() {
     const accept = useRef()
 
     const [isFormValid, setisFormValid] = useState(false)
-    const [errors ,setError] =useState([])
+    const [errors, setError] = useState({})
 
 
-    const resetForm = ()=>{
-        name.current.value =''
-        prenom.current.value=''
-        email.current.value =''
-        message.current.value =''
-        city.current.value=''
-        accept.current.checked=false
+    const resetForm = () => {
+        name.current.value = ''
+        prenom.current.value = ''
+        email.current.value = ''
+        message.current.value = ''
+        city.current.value = ''
+        accept.current.checked = false
     }
-    
 
-    const validateForms =()=>{
+
+    const validateForms = () => {
         const valueName = name.current.value
         const valuePrinom = prenom.current.value
         const valueEmail = email.current.value
@@ -33,77 +33,99 @@ export default function FormsValidation() {
         const valueCity = city.current.value
         const valueAccept = accept.current.checked
         setError([])
-        let isFormValid = true 
+        let isFormValid = true
 
 
-        if(valueName.trim() === ''){
-            setError(prevStat=> {return {...prevStat , ...{name :"filde required"} }} ) 
-            isFormValid =false
+        if (valueName.trim() === '') {
+            setError(prevStat => { return { ...prevStat, ...{ name: "filde required" } } })
+            isFormValid = false
         }
 
-        if(valuePrinom.trim() === ''){
-            setError(prevStat=> {return {...prevStat ,...{prenom :"filde required" } }}) 
-            isFormValid =false
-
-        }
-
-        if(valueEmail.trim() === ''){
-            setError(prevStat=> {return {...prevStat ,...{email :"filde required"} }} ) 
-            isFormValid =false
-
-        }else if (!valueEmail.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)){
-            setError(prevStat=> {return {...prevStat ,...{email :"Email incorrec"} }} ) 
- 
-            isFormValid =false
+        if (valuePrinom.trim() === '') {
+            setError(prevStat => { return { ...prevStat, ...{ prenom: "filde required" } } })
+            isFormValid = false
 
         }
 
-        if(valueMessage.trim() === ''){
-            setError(prevStat=>{return {...prevStat ,...{message :"filde required"}}} ) 
-            isFormValid =false
+        if (valueEmail.trim() === '') {
+            setError(prevStat => { return { ...prevStat, ...{ email: "filde required" } } })
+            isFormValid = false
+
+        } else if (!valueEmail.match(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)) {
+            setError(prevStat => { return { ...prevStat, ...{ email: "Email incorrec" } } })
+
+            isFormValid = false
 
         }
 
-        if(valueCity.trim() === ''){
-            setError(prevStat=>{return {...prevStat ,...{city :"filde required"}}} ) 
-            isFormValid =false
+        if (valueMessage.trim() === '') {
+            setError(prevStat => { return { ...prevStat, ...{ message: "filde required" } } })
+            isFormValid = false
 
         }
 
-        if(!valueAccept){
-            setError(prevStat=>{return {...prevStat ,...{Accept :"Accept All Conditions"} }} ) 
+        if (valueCity.trim() === '') {
+            setError(prevStat => { return { ...prevStat, ...{ city: "filde required" } } })
+            isFormValid = false
 
-            isFormValid =false
+        }
+
+        if (!valueAccept) {
+            setError(prevStat => { return { ...prevStat, ...{ accept: "CheckBox Must Be Checked" } } })
+            isFormValid = false
 
         }
         return isFormValid
 
     }
 
-    useEffect(()=>{
-        console.log(errors)
-    },[errors])
+    useEffect(() => {
+        // console.log(errors)
+        validateForms()
+    }, [errors])
 
-    const displayErrors =()=>{
-       return errors.map( (error ,key)=>{
-        return <li key={key}> {error.filde} : {error.message}</li>
-       })
+    const displayErrors = () => {
+        return Object.entries(errors).map((error, key) => {
+            const [filde, message] = error
+            return <li key={key}> {filde} : {message}</li>
+        })
     }
 
 
+    const fildGetError = (fildName) => {
+        return errors[fildName]
+    }
+
+    const borderError = (fildName)=>{
+        return  fildGetError(fildName) !== undefined 
+    }
+
+    const displayFildErrors = (fildName) => {
+        const border = document.querySelector(`#${fildName}`)
+        if (borderError(fildName)) {
+            border.style.border  ='1px solid red'
+            return <div className='text-danger'>{fildGetError(fildName)}</div>
+        }
+        else if (border !== null ){
+            border.removeAttribute('style')
+        }
+
+    }
+
     const HandelFilds = (e) => {
         e.preventDefault()
-       if(validateForms()){
-        setisFormValid(true)
-        resetForm()
-       }
-       
+        setisFormValid(false)
+        if (validateForms()) {
+            setisFormValid(true)
+            resetForm()
+        }
+
     }
 
 
     return <div className={'container-fluid w-75 mx-auto my-5'}>
-        
-        {isFormValid?
+
+        {isFormValid ?
             <div className="alert alert-success" role="alert">
                 <strong>success</strong> Message sent successfully !!
             </div>
@@ -111,36 +133,43 @@ export default function FormsValidation() {
             : ''}
 
         <form onSubmit={HandelFilds}>
-            {errors.length > 0 ?
-            <div className="alert alert-danger" role="alert">
-                <strong>ERROURS</strong> 
-                <ul>
-                    {displayErrors()}
-                </ul>
-            </div>
-            
-            :''}
+            {Object.keys(errors).length > 0 ?
+                <div className="alert alert-danger" role="alert">
+                    <strong>ERROURS</strong>
+                    <ul>
+                        {displayErrors()}
+                    </ul>
+                </div>
+
+                : ''}
             <div >
                 <h1>Contact Form</h1>
                 <hr />
                 <div className="mb-3">
                     <label htmlFor="name" className="form-label">Name</label>
                     <input type="text" className="form-control" id="name" placeholder="NAME" ref={name} />
+                    {displayFildErrors('name')}
                 </div>
 
                 <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Prenom</label>
+                    <label htmlFor="prenom" className="form-label">Prenom</label>
                     <input type="text" className="form-control" id="prenom" placeholder="PRENOM" ref={prenom} />
+                    {displayFildErrors('prenom')}
+
                 </div>
 
                 <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Adres Email</label>
+                    <label htmlFor="email" className="form-label">Adres Email</label>
                     <input type="text" className="form-control" id="email" placeholder="ADRES EMAIL" ref={email} />
+                    {displayFildErrors('email')}
+
                 </div>
 
                 <div className="mb-3">
-                    <label htmlFor="name" className="form-label">Message</label>
+                    <label htmlFor="message" className="form-label">Message</label>
                     <textarea className="form-control" id="message" rows="4" ref={message}></textarea>
+                    {displayFildErrors('message')}
+
                 </div>
 
                 <div className="mb-3">
@@ -152,12 +181,18 @@ export default function FormsValidation() {
                         <option value="ALG">Algeri</option>
                         <option value="EGP">Egept</option>
                     </select>
+                    {displayFildErrors('city')}
+
                 </div>
 
                 <div className="mb-3 form-check">
-                    <input type="checkbox" className="form-check-input" id="check" ref={accept} />
-                    <label htmlFor="name" className="form-label">Accept All Conditions</label>
+                    <input type="checkbox" className="form-check-input" id="accept" ref={accept} />
+                    <label htmlFor="check" className="form-label">Accept All Conditions</label>
+                    {displayFildErrors('accept')}
+
                 </div>
+
+
                 <button type="submit" className="btn btn-primary">Submit</button>
             </div>
         </form>
